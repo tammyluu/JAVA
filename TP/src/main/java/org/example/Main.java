e0e07cba111cd7b3f8b493dbc5a9514576e1d5d2
unknown <luutam_viva@yahoo.com>
2023-11-16 10:44:46 +0100

    TP

---
+++ TP/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.example.javacore.Exercise21;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+       // Exercise21.calculeCarre();
+        //Exercise21.dialogue();
+        //Exercise21.calTVA();
+        //Exercise21.isNombre();
+        Exercise21.produitSwitch();
+        //Exercise21.ordreNom();
+        }
+    }
---
+++ TP/src/main/java/org/example/javacore/Exercise21.java
@@ -0,0 +1,87 @@
+package org.example.javacore;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Exercise21 {
+    static Scanner sc = new Scanner(System.in);
+   public static  void  calculeCarre(){
+
+       System.out.print("Donne moi un nombre: ");
+       Integer nbr = sc.nextInt();
+       Integer result = nbr * nbr;
+
+       System.out.println("Voici votre resultat: " + result);
+
+   }
+
+    public static void dialogue() {
+        System.out.print("Quel est votre prénom ? ");
+        String prenom = sc.nextLine();
+        System.out.println("Bonjour, " + prenom + " !");
+   }
+
+    public static void calTVA() {
+        System.out.print("Le prix HT d'artcile est: ");
+        Double prixHT = sc.nextDouble();
+        System.out.print("Le nombre d'artcile est: ");
+        Integer nbr = sc.nextInt();
+        System.out.print("Le taux de TVA est: ");
+        Double taux = sc.nextDouble();
+        double prixTTC = prixHT * (1 + taux/100) * nbr;
+        System.out.println("Le prix total TTC correspond est: " + prixTTC);
+
+    }
+
+    public static void isNombre() {
+        System.out.print("Donne moi un nombre: ");
+        Integer nbr = sc.nextInt();
+        if (nbr > 0 ){
+            System.out.println(" Vous nombre est positif");
+        } else if (nbr < 0 ) {
+            System.out.println(" Vous nombre est negatif");
+        } else {
+            System.out.println(" Vous nombre est Zéro");
+        }
+
+
+    }
+
+    public static void produitSwitch() {
+        System.out.print("Donne moi le premier nombre: ");
+        Integer nbr1 = sc.nextInt();
+        System.out.print("Donne moi le deuxième nombre: ");
+        Integer nbr2 = sc.nextInt();
+       //int prod = nbr1* nbr2;
+        if ((nbr1> 0 && nbr2 > 0) || (nbr1 < 0 && nbr2 < 0)){
+            System.out.println(" leur produit est positif ");
+        } else if ((nbr1 < 0 && nbr2 > 0) ||(nbr1< 0 && nbr2 > 0)) {
+            System.out.println(" leur produit est negatif ");
+        } else {
+            System.out.println(" non ");
+
+
+        }
+
+
+    }
+
+    public static void ordreNom() {
+        System.out.print("Quel est votre 1er nom? ");
+        String prenom1 = sc.next();
+        System.out.print("Quel est votre 2ème nom? ");
+        String prenom2 = sc.next();
+        System.out.print("Quel est votre 3ème nom? ");
+        String prenom3 = sc.next();
+        if (prenom1.compareTo(prenom2) <= 0 && prenom2.compareTo(prenom3) <= 0) {
+            System.out.println("ils sont rangés  dans l’ordre alphabétique.");
+        } else {
+            System.out.println("ils ne sont rangés pas dans l’ordre alphabétique.");
+        }
+
+
+    }
+
+
+
+}
