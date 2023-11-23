aaf864943f83d20b8bc227f3ed92f33c87dc72db
ST_Tammy <104438570+tammyluu@users.noreply.github.com>
2023-11-23 07:05:58 +0100

    affichage

--- TpClasseChaise/src/main/java/org/example/tp_class_heritage/exo3/Compte.java
+++ TpClasseChaise/src/main/java/org/example/tp_class_heritage/exo3/Compte.java
@@ -2,17 +2,33 @@ package org.example.tp_class_heritage.exo3;
 
 public class Compte {
 
-
-
-    protected int code;
-    protected double sold;
+    public final static String TYPE_COMPTE_SIMPLE = "CS";
+    public final static String TYPE_COMPTE_EPARNGE = "CE";
+    public final static String TYPE_COMPTE_PAYANTE = "CP";
+    private static int cpt;
+    private int code;
+    private double sold;
+    private boolean decouvertAutorise;
+    private String typeCompte;
 
     public Compte() {
+
+        this.code = cpt++;
     }
 
-    public Compte(int code, double sold) {
-        this.code = code;
+    public Compte(double sold, boolean decouvertAutorise, String typeCompte) {
+        this();
         this.sold = sold;
+        this.decouvertAutorise = decouvertAutorise;
+        this.typeCompte = typeCompte;
+    }
+
+    public static int getCpt() {
+        return cpt;
+    }
+
+    public static void setCpt(int cpt) {
+        Compte.cpt = cpt;
     }
 
     public int getCode() {
@@ -31,11 +47,28 @@ public class Compte {
         this.sold = sold;
     }
 
+    public boolean isDecouvertAutorise() {
+        return decouvertAutorise;
+    }
+
+    public void setDecouvertAutorise(boolean decouvertAutorise) {
+        this.decouvertAutorise = decouvertAutorise;
+    }
+
+    public String getTypeCompte() {
+        return typeCompte;
+    }
+
+    public void setTypeCompte(String typeCompte) {
+        this.typeCompte = typeCompte;
+    }
+
     @Override
     public String toString() {
-        return "Compte{" +
-                "code=" + code +
-                ", sold=" + sold +
-                '}';
+        return "|\t" + StringHelpers.leftPad(code + "", 11, "0") + "\t|\t" + typeCompte + "\t|\t"
+                + sold + "\t|\t" + decouvertAutorise + "\t|\t" +  "\t|\t";
+
     }
+
+
 }
---
+++ TpClasseChaise/src/main/java/org/example/tp_class_heritage/exo3/view/Principal.java
@@ -0,0 +1,29 @@
+package org.example.tp_class_heritage.exo3.view;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    private static String menuPrincipalDisplay(){
+            String choix;
+            System.out.println(
+                    "------------------------------- MENU BANQUE CDA ------------------------------------------------");
+            /*System.out.println("1 -	Creer une agence ");
+            System.out.println("2 -	Creer un client ");
+            System.out.println("3 -	Creer un compte bancaire ");*/
+            System.out.println("4 -	Recherche de compte (numero de compte) ");
+            System.out.println("5 -	Recherche de client (Nom, Numero de compte, identifiant de client)");
+            System.out.println("6 -	Afficher la liste des comptes d'un client (identifiant client)");
+            System.out.println("7 -	Imprimer les infos client (identifiant client)");
+            System.out.println("10-	Afficher tous les comptes bancaires");
+            System.out.println("11-	Quitter le programme ");
+            System.out.println(
+                    "---------------------------------------------------------------------------------------------------------------------");
+            System.out.print("\tChoix   ");
+            choix = Principal.sc.nextLine();
+            return choix;
+        }
+    }
+}
