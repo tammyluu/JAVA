629ffd345a76505fb72eca03ad97527ccff51298
unknown <luutam_viva@yahoo.com>
2024-01-04 17:26:46 +0100

    Demo6

--- JPA/demo/demo_jpa/src/demo_jpa.sql
+++ JPA/demo/demo_jpa/src/demo_jpa.sql
@@ -3,3 +3,8 @@ use demo_jpa;
 select * from Person;
 select * from maison;
 select * from address;
+select * from departement;
+select * from employee;
+select * from post;
+select * from tag;
+select * from post_tag;
