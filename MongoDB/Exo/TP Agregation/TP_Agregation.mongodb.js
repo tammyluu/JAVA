c0f98e76496ed5bcfceb3d2546197d734134f3b7
ST_Tammy <104438570+tammyluu@users.noreply.github.com>
2024-01-17 13:32:02 +0100

    TP agregation 2

--- MongoDB/Exo/TP Agregation/TP_Agregation.mongodb.js
+++ MongoDB/Exo/TP Agregation/TP_Agregation.mongodb.js
@@ -1,8 +1,10 @@
 use("livre")
 
 // --1.	Trier les publications de « Toru Ishida » par titre de livre et par page de début.
-// db.livres.aggregate([{$match: { authors: "Toru Ishida" }},
+// db.livres.aggregate([{$match: { authors: "Toru Ishida", type: "Article"}},
 //  {$sort: {title : 1, "page.start" : 1 }}])
+//--Projeter le résultat sur le titre de la publication, et les pages.
+
 
 // --3.	Compter le nombre de ses publications.
 // db.livres.aggregate([{$match: { authors: "Toru Ishida" }},
@@ -11,11 +13,11 @@ use("livre")
 
 // -- 4.	Compter le nombre de publications depuis 2011 et par type.
 // db.livres.aggregate([
-//     { $match: { "year": { $gte: 2011 } } },
+//     { $match: { "year": { $gt: 2011 } } },
 //      { $group: {_id: "$type",totalPublications: { $sum: 1 }}}
 //   ])
   
 
 // --5.	Compter le nombre de publications par auteur et trier le résultat par ordre croissant.
-db.livres.aggregate([{ $group: {_id: "$authors",totalPublications: { $sum: 1 }}}, 
+db.livres.aggregate([{$unwind : "$authors"},{ $group: {_id: "$authors",totalPublications: { $sum: 1 }}}, 
 {$sort: {totalPublications: 1}}])
